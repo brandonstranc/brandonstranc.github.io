f5e70a94dc5c771a349602c9ef3af7f1da077f10
brandonstranc <32006313+brandonstranc@users.noreply.github.com>
2017-09-15 23:20:58 -0500

    First Upload

---
+++ _js/bootstrapScript.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$("nav").addClass("animated bounceInDown");
+	$(".fade-in").addClass("animated slideInLeft");
+
+
+
+
+
+	// $('.slide-section').click(function(e) {
+
+	// 	var linkHref = $(this).attr('href');
+
+	// 	$('html, body').animate( {
+	// 		scrollTop: $(linkHref).offset().top
+	// 	}, 1000);
+
+	// 	e.preventDefault();
+	// });
+});
